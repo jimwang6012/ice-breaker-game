e3f71a8a242058d173d61012e812031bd065786f
nalydnix <dylanxin@outlook.com>
2022-05-04 23:34:41 +1200

    fixed PORT

--- packages/frontend/src/Socket.js
+++ packages/frontend/src/Socket.js
@@ -1,6 +1,6 @@
 import socketClient from "socket.io-client";
 
-const PORT = process.env.PORT || "8080";
+const PORT = 43179;
 const SERVER = "https://ice-breaking-game.herokuapp.com:" + PORT;
 
 // the socketClient object must be unique globally, and only instantiated once
