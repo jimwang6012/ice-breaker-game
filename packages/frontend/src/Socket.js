b5ca52b7578e7e63eeee633bb2a2d5a9042eeb34
nalydnix <dylanxin@outlook.com>
2022-05-04 23:21:48 +1200

    fix SERVER address

--- packages/frontend/src/Socket.js
+++ packages/frontend/src/Socket.js
@@ -1,5 +1,7 @@
 import socketClient from "socket.io-client";
-const SERVER = "http://localhost:8080/";
+
+const PORT = process.env.PORT || 8080;
+const SERVER = "https://ice-breaking-game.herokuapp.com/" + String(PORT);
 
 // the socketClient object must be unique globally, and only instantiated once
 export default socketClient(SERVER);
