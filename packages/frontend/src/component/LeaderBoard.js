0037fb1b3f8aa4f51c1f598176eb4251a4aeb363
GlockTian <61868315+GlockTian@users.noreply.github.com>
2022-05-15 00:25:42 +1200

    fix width of modal (#109)

--- packages/frontend/src/component/LeaderBoard.js
+++ packages/frontend/src/component/LeaderBoard.js
@@ -7,25 +7,27 @@ import "./LeaderBoard.css";
 export function LeaderBoard({ list, myID }) {
   return (
     <div>
-      <div className="mt-5 w-full h-0.5 bg-white"></div>
+      <div className="mt-5 w-96 h-0.5 bg-white"></div>
 
       {/* Titles */}
       <div className="flex flex-row pt-2">
-        <div className="flex flex-row justify-around w-1/3">
+        <div className="w-1/4">
           <Text size="xl" color="white" weight={500}>
             Ranking
           </Text>
         </div>
-        <div className="flex flex-row justify-around w-2/3">
+        <div className="w-1/2 flex flex-row justify-center">
           <Text size="xl" color="white" weight={500}>
             Player
           </Text>
+        </div>
+        <div className="w-1/4  flex flex-row justify-center">
           <Text size="xl" color="white" weight={500}>
             Score
           </Text>
         </div>
       </div>
-      <div className="wholeBoard">
+      <div>
         {list !== undefined ? (
           list.map((p, index) => (
             <RankItem player={p} rank={index + 1} myID={myID} key={index} />
@@ -55,7 +57,7 @@ function RankItem({ player, rank, myID }) {
             {rank}.
           </Text>
         </div>
-        <div className="flex flex-row justify-center w-1/2 ml-5">
+        <div className="flex flex-row justify-center align-middle w-1/2">
           <Avatar
             src={
               player.isBreaker
@@ -71,10 +73,10 @@ function RankItem({ player, rank, myID }) {
             }}
           />
 
-          <div className="pt-1 username">{player.name}</div>
+          <div className="pl-2 pt-1 ">{player.name}</div>
         </div>
         <div className="flex flex-row justify-center w-1/4">
-          <Text size="xl" weight={500} className="mr-10">
+          <Text size="xl" weight={500}>
             {player.score}
           </Text>
         </div>
