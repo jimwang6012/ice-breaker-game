681573b63c5d934983f580504f3eb79a34780e86
nalydnix <dylanxin@outlook.com>
2022-05-15 14:30:36 +1200

    change path

--- packages/frontend/src/Socket.js
+++ packages/frontend/src/Socket.js
@@ -3,6 +3,6 @@ import { io } from "socket.io-client";
 // the socketClient object must be unique globally, and only instantiated once
 export default io(
   process.env.NODE_ENV === "production"
-    ? "https://ice-breaker-alpha0.herokuapp.com/"
+    ? "https://break-ice-game.herokuapp.com/"
     : "http://localhost:8080"
 );
