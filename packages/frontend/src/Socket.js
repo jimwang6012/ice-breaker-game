6cde65403d47889f209f130545aaec7589c53b68
jimwang6012 <68952466+jimwang6012@users.noreply.github.com>
2023-03-21 11:04:09 +0000

    Fixed socketIO URL

--- packages/frontend/src/Socket.js
+++ packages/frontend/src/Socket.js
@@ -3,6 +3,6 @@ import { io } from "socket.io-client";
 // the socketClient object must be unique globally, and only instantiated once
 export default io(
   process.env.NODE_ENV === "production"
-    ? "https://ice-breakers-game.herokuapp.com/"
+    ? "http://ec2-13-236-207-49.ap-southeast-2.compute.amazonaws.com/"
     : "http://localhost:8080"
 );
